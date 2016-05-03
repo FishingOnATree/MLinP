d3fe57ba0001f43e4fefda9ed8768b17aeddeb77
FishingOnATree <raysung78@gmail.com>
2016-05-03 11:08:43 +0800

    1. implemented ex3 training method 2. upgraded cost_function to handle log0 cases

--- python/ex3/ex3.py
+++ python/ex3/ex3.py
@@ -3,16 +3,40 @@ __author__ = 'Rays'
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as op
+from util_functions import util_functions as ut
 
 input_layer_size  = 400;  # 20x20 Input Images of Digits
 num_labels = 10;          # 10 labels, from 1 to 10
                           # (note that we have mapped "0" to label 10)
-
+l = 0.1
 
 def show_images(sample):
     plt.imshow(X[sample, 1:].reshape(-1, 20).T)
     plt.axis('off')
     plt.show()
+
+
+def gradient_function(theta, x, y, lmda):
+    m, n = x.shape
+    theta = theta.reshape((n, 1))
+    h = ut.sigmoid_function(np.dot(x, theta)).reshape(y.shape)
+    lambda_adj = np.multiply(theta, lmda/m).reshape(theta.shape)
+    lambda_adj[0] = 0 #do not touch theta0
+    return np.divide(np.dot(x.transpose(), np.subtract(h, y)), m) + lambda_adj
+
+
+def one_vs_all(x, y, l):
+    initial_theta = np.random.rand(x.shape[1], 1)
+    all_theta = np.zeros((num_labels, x.shape[1]))
+    for k in range(num_labels):
+        classifier = (y == (k+1)) * 1
+        # cost = ut.logistic_cost_function(initial_theta, x, classifier, l)
+        # print("Initial cost = %f" % cost)
+        ops = {"maxiter": 5000}
+        result = op.minimize(fun=ut.logistic_cost_function, x0=initial_theta, args=(x, y, l), method='TNC', jac=gradient_function, options=ops)
+        print('%d result = %s' % (k+1, result.success))
+    print(all_theta.shape)
+    return all_theta
 
 
 X = np.load('X.npy')
@@ -22,4 +46,5 @@ theta1 = np.load('theta1.npy')
 theta1 = np.load('theta2.npy')
 
 sample = np.random.choice(X.shape[0], 20)
-show_images(sample)
+#show_images(sample)
+one_vs_all(X, y, l)
--- python/util_functions/util_functions.py
+++ python/util_functions/util_functions.py
@@ -7,7 +7,7 @@ def sigmoid_function(z):
     return (1.0 / (1.0 + np.exp(-z))).reshape(z.shape)
 
 
-def logistic_cost_function(theta, x, y, l):
+def logistic_cost_function(theta, x, y, l, neg_inf_replace= -10000):
     '''
     theta: n x 1 vector
     x: m x n matrix
@@ -18,5 +18,8 @@ def logistic_cost_function(theta, x, y, l):
     h = sigmoid_function(np.dot(x, theta))
     lambda_cost_adj = l / 2 / m * np.power(theta, 2)
     lambda_cost_adj[0] = 0  # theta 0 should not be adjusted
-    diff_sum = np.multiply(y, np.log(h)) + np.multiply(np.add(1, np.negative(y)), np.log(np.add(1, np.negative(h))))
-    return sum(diff_sum) / -m + l / 2 / m * sum(np.power(lambda_cost_adj, 2))
+    # substitue -Inf with a large negative constant
+    inner_term = np.log(1 - h)
+    inner_term[np.isneginf(inner_term)] = neg_inf_replace
+    diff_sum = np.multiply(y, np.log(h)) + np.multiply(1 - y, inner_term)
+    return (sum(diff_sum) / -m + l / 2 / m * sum(np.power(lambda_cost_adj, 2)))[0]
